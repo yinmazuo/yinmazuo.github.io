57f33cd9d264c20029a42d8c98327467824bd08e
马世祥 <2223673415@qq.com>
2016-04-05 23:30:57 +0800

    修复task——sk_2_23

--- ifetask/task2/task_2_23/task_2_23.js
+++ ifetask/task2/task_2_23/task_2_23.js
@@ -26,8 +26,6 @@ window.onload = function () {
 			showQueue = []; //清空队列
 			clearTimeout(timer); //清除定时器
 		}
-
-		/*eval(e.target.id + "Order(root);");*/
 		switch (e.target.id) {  //判断点击的按钮是哪个
 			case "pre" : 
 			preOrder(root);										
@@ -35,28 +33,35 @@ window.onload = function () {
 			case "post" : 					
 			postOrder(root);										
 			break;
-			case "search" : 					
+			case "search" : 
+			if (searchKey.value == "") {
+				alert("请输入搜索内容！");
+				return;
+			}
+			var value = searchKey.value;					
 			preOrder(root);		//遍历搜索的方法随意写的，也可以在遍历的过程中输入文字，同样会有效								
 			break;
 		}
 		show(); //showQueue中待显示节点入队完毕并开始显示
-
 		function show() {
 			head = showQueue.shift(); //出队
-
 			if (head) {
 				head.style.backgroundColor = "#ff0000";//显示色
 				timer = setTimeout(function(){
 					head.style.backgroundColor = "#fff";//节点的红色变为白色
 					show(); //递归调用show，使要显示的节点不停出队显示，直至为空
 				}, 500);
-
-				if (head.firstChild.nodeValue.trim() == searchKey.value) {//比较遍历的节点中的文字与输入框文字
-					showQueue = []; //清空队列
-					clearTimeout(timer); //清除定时器
+				if (value) {
+					if (head.firstChild.nodeValue.trim() == value) {//比较遍历的节点中的文字与输入框文字						
+						showQueue = []; //清空队列
+						clearTimeout(timer); //清除定时器
+						return ;
+					}
+					if (showQueue.length == 0) {
+						alert("为搜索到内容！");
+					}
 				}
-			} 
-			
+			} 		
 		}
 
 	};
