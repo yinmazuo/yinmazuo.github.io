3e7a6acef718840c03366d4560cddbea2ec96cb9
马世祥 <2223673415@qq.com>
2016-04-05 23:13:18 +0800

    修复task_2_25

--- ifetask/task2/task_2_25/task_2_25.js
+++ ifetask/task2/task_2_25/task_2_25.js
@@ -1,34 +1,30 @@
 window.onload = function () {
 	var checkedNodeParent, checkedNode;
 
-	$("#root").onclick = function (e) {
+	$("#wrap").onclick = function (e) {
 		//判断选项卡是否已存在
-		if (this.getElementsByClassName("tooltips").length > 0) {
-			checkedNode = checkedNodeParent.getElementsByClassName("tooltips")[0];
+		if (document.getElementsByClassName("tooltips").length > 0) {
+			checkedNode = document.getElementsByClassName("tooltips")[0];
 			//点击选项卡
 			switch (e.target.id) {
 				case "addChildNode":
-				addChildNode(checkedNodeParent);
+				addChildNode(checkedNode.parentNode);
 				break;
 				case "removeNode":
-				removeNode(checkedNodeParent);
+				removeNode(checkedNode.parentNode);
 				break;
-				case "fold":
-				fold(checkedNodeParent);
-				break;
-				case "spread":
-				spread(checkedNodeParent);
-				break;
-				default: ;
+				default:
+				checkedNode.parentNode.removeChild(checkedNode);
 			}
-			//选项卡消失
-			checkedNodeParent.removeChild(checkedNode);
 		}
-		//判断并显示选项卡
+		//
 		if (e.target.tagName == "SPAN") {
-			checkedNodeParent = e.target.parentNode;		
-			e.target.parentNode.appendChild(tooltips());
+			checkedNodeParent = e.target.parentNode;
+			change(checkedNodeParent);			
 		}
+		if (e.target.tagName == "I") {
+			e.target.parentNode.appendChild(tooltips());
+		}		
  	};
 
  	//搜索
@@ -58,7 +54,9 @@ function preOrder(node) {
 		preOrder(node.nextElementSibling);
 	}
 }
-//查找到后显示
+/*
+*查找到后显示
+*/
 function show(node) {
 	if (!(node.parentNode == null)) {//递归向上，使祖先节点都展开显示
 		show(node.parentNode);
@@ -67,15 +65,15 @@ function show(node) {
 		}	
 	}	
 }
-//添加
+/*
+*添加
+*/
 function addChildNode(node) {
 	var ul, li, span, name = prompt("请输入节点名：");
-	if (name == "") {alert("节点名不能为空！");}
+	if (!name) {alert("节点名不能为空！");return;}
 	ul = document.createElement("ul");
 	li = document.createElement("li");
-	span = document.createElement("span");
-	span.innerHTML = name;
-	li.appendChild(span);
+	li.innerHTML = "<span>" + name + "</span><i>=</i>";
 	ul.appendChild(li);
 
 	if (node.getElementsByTagName("ul").length > 0) {//为了结构的完整性
@@ -83,8 +81,11 @@ function addChildNode(node) {
 	} else {
 		node.appendChild(ul);
 	}
+	node.getElementsByTagName("ul")[0].style.display = "";
 }
-//删除
+/*
+*删除
+*/
 function removeNode(node) {
 	if (node.parentNode.id && node.parentNode.id == "root") {
 		alert("不能删除根节点！");
@@ -92,26 +93,34 @@ function removeNode(node) {
 	}
 	node.parentNode.removeChild(node);
 }
-
-function change(node, status) {
+/*
+*改变状态
+*/
+function change(node) {	
 	if (node.getElementsByTagName("ul").length > 0) {
-		node.getElementsByTagName("ul")[0].style.display = status;
+		var display = node.getElementsByTagName("ul")[0].style.display;
+		if (display == "none") {
+			node.getElementsByTagName("ul")[0].style.display = "";
+		} else {
+			node.getElementsByTagName("ul")[0].style.display = "none";
+		}
 	}
 }
-//折叠
-function fold(node) {
-	change(node, "none");
-}
-//展开
-function spread(node) {
-	change(node, "");
-}
-//选项卡
+
+/*
+*选项卡
+*/
 function tooltips() {
-	var tooltips = $("#tool > .tooltips").cloneNode(true);//从辅助节点那里深度克隆出选项卡的完整节点
-	return tooltips;
+	var fragment = document.createDocumentFragment(),
+		tooltips = document.createElement("div");
+	tooltips.className = "tooltips";
+	tooltips.innerHTML = "<div id='addChildNode'>添加</div><div id='removeNode'>删除</div><div id='cancle'>取消</div>"; 
+	fragment.appendChild(tooltips); 
+	return fragment;
 }
-//选择器
+/*
+*选择器
+*/
 function $(selector) {
 	return document.querySelector(selector);
 }
